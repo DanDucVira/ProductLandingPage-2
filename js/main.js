21fa9e1817f1fbdc1d95e5e6f336e24e32a61724
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-04-05 13:04:12 -0400

    Finished the project

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
         document.querySelector('#nav-bar').style.backgroundColor = "#41aaa8";
         document.querySelector('.btn-green').style.backgroundColor = "#b5525c";
-        document.querySelector('.btn-green').style.color = "black";
+        document.querySelector('.btn-green').style.color = "white";
     } else {
         document.querySelector('#nav-bar').style.backgroundColor = "transparent";
         document.querySelector('.btn-green').style.backgroundColor = "#41aaa8";
-        document.querySelector('.btn-green').style.color = "white";
+
     }
 });
